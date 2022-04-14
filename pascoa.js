9c1c4af1831969f793ab95b8e55f182c46906f39
gbHenderson <102072061+gbHenderson@users.noreply.github.com>
2022-04-14 12:05:50 -0300

    fim?

--- pascoa.js
+++ pascoa.js
@@ -9,10 +9,11 @@ foto.style.backgroundImage = `url(pas${++n}.jpg)`
 foto.style.backgroundPosition = ('center center')
 foto.style.backgroundSize = ('cover')}
 
-    else if(n >= 7 && data == ''){
+    else if(n >= 7 && data == '26'){
 foto.innerText = 'Te amo!'
 foto.style.color = 'red'
-foto.style.lineHeight = '730px'
+foto.style.lineHeight = '45px'
+foto.style.fontSize = '60px'
 foto.style.backgroundImage = `url(amor.jpg)
 `}
 
@@ -24,18 +25,21 @@ foto.style.backgroundImage = `url(mae.jpg)`}
   
     else{
         foto.style.backgroundImage = 'url(pas8.jpg)'
-        foto.innerText = 'Saúde!'
-    }}
+        foto.innerText = 'Saúde!'}}
 
-if (nome == '' || nome == 'marcella'){
+if (nome == 'Marcella' || nome == 'marcella'){
     var data = window.prompt('Se você é mesmo a marcella. Digite o dia do nosso namoro:')
-    if (data == ''){
+    if (data == '26'){
         document.getElementsByTagName('h1')[0].innerText = `Feliz Páscoa pra você, ${nome}!`
-        document.getElementsByTagName('p')[0].innerHTML = `Meu amor, tudo que eu tinha para falar e desabafar contigo eu já disse na cartinha, então aqui fica só umas considerações de eu te desejando uma feliz páscoa, ${nome}! Você sabe que eu te amo muito que sou muito feliz de ter sua presença quando você está do meu lado, que sou muito feliz de ter você como a minha namorada e que espero passar todas as páscoas da minha vida ao seu lado. Tenho uma gratidão enorme de poder ter achado alguém que faz eu me sentir 100% completado quando estou a seu lado, além de obviamente ser grato a pessoa que você e que me ajuda demais e me apoia. Vivemos momentos complicados ultimamente, porém eu tenho certeza que isso foi para nos fortalezer e fazer com que evoluamos! <br><br>
-        Hoje, na páscoa, é um dia de receber chocolate, mas o mais belo e melhor chocolate que eu já provei é a sua pele cor de chocolate ao leite que eu acabo me deliciando e sentindo um prazer incomensurável, com o simples fato de eu passar a mão, boca e provando do melhor chocolate do mundo.`}
+        document.getElementsByTagName('p')[0].innerHTML = `Meu amor, tudo que eu tinha para falar e desabafar contigo eu já disse na cartinha, então aqui fica só umas considerações de eu te desejando uma feliz páscoa, ${nome}! Você sabe que eu te amo muito que sou muito feliz de ter sua presença quando você está do meu lado, que sou muito feliz de ter você como a minha namorada e que espero passar todas as páscoas da minha vida ao seu lado. Tenho uma gratidão enorme de poder ter achado alguém que faz eu me sentir 100% completado quando estou a seu lado, além de obviamente ser grato a pessoa que você é, que me ajuda demais e me apoia. Vivemos momentos complicados ultimamente, porém eu tenho certeza que isso foi para nos fortalezer e fazer com que evoluamos! <br><br>
+        Hoje, na páscoa, é um dia de receber chocolate, mas o mais belo e melhor chocolate que eu já provei é a sua pele cor de chocolate ao leite que eu acabo me deliciando e sentindo um prazer incomensurável, com o simples fato de eu passar a mão, boca e acabao sempre provando do melhor chocolate do mundo.`}
     
-      else(document.getElementsByTagName('p')[0].innerHTML = 'VOCÊ NÃO É A MARCELLA!')
-}
+      else(document.getElementsByTagName('p')[0].innerHTML = 'VOCÊ NÃO É A MARCELLA VERDADEIRA!')
+      document.getElementsByTagName('h1')[0].innerText = `Feliz Páscoa pra você, ${nome}`}
+
+else if(nome == 'juliana' || nome == 'Juliana'){
+    document.getElementsByTagName('h1')[0].innerText = `Feliz Páscoa pra você, Amigão!`
+    document.getElementsByTagName('p')[0].innerHTML = `Te amo, mamãe! Uma ótima páscoa pra você e que seu dia seja maravilhoso. Aproveite seus bombons logo, antes que um ratinho coma tudo, hein...`}
 
 else{
 document.getElementsByTagName('h1')[0].innerText = `Feliz Páscoa pra você, ${nome}!`
